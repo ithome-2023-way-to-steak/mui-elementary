6d8033e403d4586fbd9a9cee7217cf69a0d5bee3
Charlie <charlie@lawsnote.com>
2023-01-05 15:27:34 +0800

    [feat] add declare module

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,1 +1,26 @@
 /// <reference types="react-scripts" />
+/// <reference path="@mui/material/Button" />
+/* /// <reference path="@mui/material/styles" /> */
+
+import '@mui/material/styles';
+
+declare module '@mui/material/Button' {
+  interface ButtonPropsSizeOverrides {
+    montaBig: true;
+  }
+  interface ButtonPropsColorOverrides {
+    neutral: true;
+  }
+  interface ButtonPropsVariantOverrides {
+    monta: true;
+  }
+}
+
+declare module '@mui/material/styles' {
+  interface Palette {
+    neutral: PaletteColor;
+  }
+  interface PaletteOptions {
+    neutral?: PaletteColorOptions;
+  }
+}
